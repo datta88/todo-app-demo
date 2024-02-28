19175fded2e2b2d65157369b21f90b9453e764f8
datta88 <dattatupe77@gamil.com>
2024-02-28 18:01:21 +0530

    added image delete

--- datta/src/componets/TodoList.js
+++ datta/src/componets/TodoList.js
@@ -1,11 +1,13 @@
 import React from 'react'
-
+import './TodoList.css'
 function TodoList(props) {
   return (
     <div>
         <li>
             {props.item}
-            <span onClick={e=>{props.deleteItem(props.index)}}>X</span>
+            <span onClick={e=>{props.deleteItem(props.index)}}>
+              <img src='./image/delete.png' className='delete-img' />
+            </span>
         </li>
     </div>
   )
